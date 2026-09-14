a425737c1b498546534ffa300a9680ad1ce1c7bb
Andrew Boldyrev <angelcorpc@ya.ru>
2026-09-14 15:10:54 +0700

    Add personal data revision metadata and archive link

--- resources/views/legal/personal-data.blade.php
+++ resources/views/legal/personal-data.blade.php
@@ -14,7 +14,8 @@
         <p class="eyebrow"><span></span> Документы</p>
         <h1>Политика обработки персональных данных</h1>
         <p>Правила обработки персональных данных при использовании Cropkeeper.</p>
-        <span class="legal-updated">Редакция от 5 сентября 2026 года</span>
+        <span class="legal-updated">{{ $legalRevision['label'] }}</span>
+        <a class="legal-archive-link" href="{{ route('legal.archive.index', ['document' => $documentCode]) }}">Архив редакций</a>
     </div>
 </section>
 
