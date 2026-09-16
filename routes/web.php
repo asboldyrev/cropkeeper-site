612627ed69f633514845b82ef4963a88e1f7020c
Andrew Boldyrev <angelcorpc@ya.ru>
2026-09-16 18:17:45 +0700

    Use static robots file for web server compatibility

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,6 @@ Route::get('/sitemap.xml', function () {
         ], 200, ['Content-Type' => 'application/xml; charset=UTF-8']);
 })->name('sitemap');
 
-Route::get('/robots.txt', function () {
-    $canonicalBaseUrl = rtrim((string) config('app.url'), '/');
-
-    return response("User-agent: *\nDisallow:\n\nSitemap: {$canonicalBaseUrl}/sitemap.xml\n", 200, [
-        'Content-Type' => 'text/plain; charset=UTF-8',
-    ]);
-})->name('robots');
-
 Route::get('/agreement', [LegalDocumentController::class, 'agreement'])->name('agreement');
 Route::get('/offer', [LegalDocumentController::class, 'offer'])->name('offer');
 Route::get('/personal-data', [LegalDocumentController::class, 'personalData'])->name('personal-data');
